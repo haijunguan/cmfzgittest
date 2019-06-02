acd399dae87462402b159663ab7a431d1cb44976
ghj <haijunguan@163.com>
2019-06-02 10:17:27 +0800

    this is version 2

--- src/main/java/com/example/CmfzgittestApplication.java
+++ src/main/java/com/example/CmfzgittestApplication.java
@@ -8,9 +8,6 @@ public class CmfzgittestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CmfzgittestApplication.class, args);
-        System.out.println();
-        System.out.println("hehe...");
-        System.out.println();
     }
 
 }
---
+++ src/main/java/com/example/com/test/Testgit.java
@@ -0,0 +1,7 @@
+package com.example.com.test;
+
+public class Testgit {
+    public static void main(String[] args) {
+        System.out.println("Hi,baby.");
+    }
+}
