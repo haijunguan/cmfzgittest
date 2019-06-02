b529a4ffef4901d4fd30f9f9b98be160054ca53f
ghj <haijunguan@163.com>
2019-06-02 10:19:43 +0800

    version 1

--- src/main/java/com/example/com/test/Testgit.java
+++ src/main/java/com/example/com/test/Testgit.java
@@ -3,5 +3,6 @@ package com.example.com.test;
 public class Testgit {
     public static void main(String[] args) {
         System.out.println("Hi,baby.");
+        System.out.println("Hello,baby,I love you.");
     }
 }
