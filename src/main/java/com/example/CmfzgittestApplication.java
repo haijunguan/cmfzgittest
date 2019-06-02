ce55d94134ceeb2d091beee03f09514507704696
haijunguan <51193538+haijunguan@users.noreply.github.com>
2019-06-02 09:51:20 +0800

    Update CmfzgittestApplication.java

--- src/main/java/com/example/CmfzgittestApplication.java
+++ src/main/java/com/example/CmfzgittestApplication.java
@@ -9,6 +9,7 @@ public class CmfzgittestApplication {
     public static void main(String[] args) {
         SpringApplication.run(CmfzgittestApplication.class, args);
         System.out.println();
+        System.out.println("hehe...");
     }
 
 }
